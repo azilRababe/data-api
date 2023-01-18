a524dd33fbd8dab1e50324268b58206b2b71b4d9
rababe azil <azilrababe@gmail.com>
2023-01-18 16:07:44 +0100

    ...

--- weather.py
+++ weather.py
@@ -4,25 +4,31 @@ import sys
 import urllib.parse
 import requests
 
-BASE_URI = "https://weather.lewagon.com"
+BASE_URI = "https://weather.lewagon.com/geo/1.0/direct?q="
 
 
 def search_city(query):
     '''Look for a given city. If multiple options are returned, have the user choose between them.
        Return one city (or None)
     '''
-    pass  # YOUR CODE HERE
+    res= requests.get(BASE_URI+query).json()
+    return res[0]
+
 
 def weather_forecast(lat, lon):
     '''Return a 5-day weather forecast for the city, given its latitude and longitude.'''
-    pass  # YOUR CODE HERE
+    URL=f'https://weather.lewagon.com/data/2.5/forecast?lat={lat}&lon={lon}'
+    return (requests.get(URL).json()['list'][0]['weather'][0]['main'])
+
+
 
 def main():
     '''Ask user for a city and display weather forecast'''
     query = input("City?\n> ")
     city = search_city(query)
     # TODO: Display weather forecast for a given city
-    pass  # YOUR CODE HERE
+    forecast=weather_forecast(city['lat'],city['lon'])
+    print(forecast)
 
 if __name__ == '__main__':
     try:
